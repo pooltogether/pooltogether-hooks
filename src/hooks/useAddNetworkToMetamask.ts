281df1bd27e5a589612c8e1e3e3fa187fce88b10
Charles Bergeron <chuck.bergeron@gmail.com>
2021-08-26 15:26:42 -0700

    * allow network switching to ethereum

--- src/hooks/useAddNetworkToMetamask.ts
+++ src/hooks/useAddNetworkToMetamask.ts
@@ -2,18 +2,31 @@ import { getChain, formatNetworkForAddEthereumChain } from '@pooltogether/evm-ch
 
 export const useAddNetworkToMetamask = (chainId) => {
   return async () => {
+    const ethereum = (window as any).ethereum
+
+    const network = getChain(chainId)
+    const formattedNetwork = formatNetworkForAddEthereumChain(network)
+
+    const basicFormattedNetwork = { chainId: formattedNetwork.chainId }
+
     try {
-      const ethereum = (window as any).ethereum
-
-      const network = getChain(chainId)
-      const formattedNetwork = formatNetworkForAddEthereumChain(network)
-
       await ethereum.request({
-        method: 'wallet_addEthereumChain',
-        params: [formattedNetwork]
+        method: 'wallet_switchEthereumChain',
+        params: [basicFormattedNetwork]
       })
     } catch (error) {
-      console.error(error)
+      // This error code indicates that the chain has not been added to MetaMask.
+      if (error.code === 4902) {
+        try {
+          await ethereum.request({
+            method: 'wallet_addEthereumChain',
+            params: [formattedNetwork]
+          })
+        } catch (addError) {
+          console.error('error trying to add chain')
+        }
+      }
+      // handle other "switch" errors
     }
   }
 }
